1a26f5de44eceb39eb4ad3c3605e7d74b8e92863
joaoeymard <joaoeymard@gmail.com>
2022-10-27 00:14:08 -0300

    Corrigindo problema de reconhecimento de click

--- scripts/script.js
+++ scripts/script.js
@@ -12,12 +12,8 @@ pantonFont.load().then(function (font) {
 const baseImage = new Image();
 baseImage.src = "./apoioBolsonaro22.png";
 
-document
-  .querySelector("button[name=save]")
-  .addEventListener("click", saveImage);
-document
-  .querySelector("button[name=share]")
-  .addEventListener("click", shareImage);
+document.getElementById("btnSave").addEventListener("click", saveImage);
+document.getElementById("btnShare").addEventListener("click", shareImage);
 
 function roundRect(ctx, x, y, width, height, radius = 5) {
   if (typeof radius === "number") {
